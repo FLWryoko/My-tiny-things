bb2300baf6d1a8eca88f4476d26e19307ec79afb
FLWryoko <example>
2018-10-19 11:57:42 +0000

    BUG:Fixed wrong calculation.Changed accept data type is only int.

--- gamma.py
+++ gamma.py
@@ -1,19 +1,18 @@
-from math import factorial
 from typing import cast
 
-n=0
-m=0
 print('range:')
 stitr = input()
+
 itr = int(stitr)
+
+fac =1
+facc =1
+
+print('x ,y')
 for count in range(itr):
-    if count==0:
-        print('x')
-        n = input() 
-        m = float(n)
-    gamma = m*(m-1)
-    stgamma = str(gamma)
-    stm = str(m)
-    print('x:m'+ stm+ 'y:'+stgamma)
-    m = m-0.1
-    count + 1
+   fac = fac * facc
+   print(str(count+1)+ ','+str(fac))
+   facc = facc + 1
+   count = count + 1
+
+
